84f56134a37a2db7d5270ee0d197f05d3a139990
Juan Jose Herrera <juanjose.herrera.ocufile@outlook.com>
2020-06-11 19:37:23 -0500

    slint fixes

--- index.test.js
+++ index.test.js
@@ -1,11 +1,11 @@
-var isGeo = require('./index');
+var isGeo = require('./index')
 
-describe('Testing isGeo', function(){
-    it('should return true passing Geo', function(){
-        expect(isGeo('Geo')).toBeTruthy();
-    });
+describe('Testing isGeo', function () {
+  it('should return true passing Geo', function () {
+    expect(isGeo('Geo')).toBeTruthy()
+  })
 
-    it('should return false passing Google', function(){
-        expect(isGeo('Goolge')).toBeFalsy();
-    });
-});
+  it('should return false passing Google', function () {
+    expect(isGeo('Goolge')).toBeFalsy()
+  })
+})
